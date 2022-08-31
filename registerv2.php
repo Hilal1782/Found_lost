90f8be73221904b0ff517bae57e53a24b04551c8
Hilal Ahmad <hilal191782@gmail.com>
2022-08-31 23:41:17 +0500

    Update registerv2.php

--- registerv2.php
+++ registerv2.php
@@ -1,10 +1,5 @@
 <?php
-
-
-
-
 include 'db.php';
-
 if(isset($_POST['submit'])){
 	$fname = $_POST['fname'];
 	$lname = $_POST['lname'];
@@ -13,22 +8,14 @@ if(isset($_POST['submit'])){
 	$pwd = $_POST['pwd'];
 	$identity = $_POST['identity'];
 	$file = $_FILES['image'];
-
 	//print_r($file);
-
 	$filename = $file['name'];
 	$filepath = $file['tmp_name'];
 	$fileerror = $file['error'];
-
-	if($fileerror == 0){
-		
+	if($fileerror == 0){	
 		$destfile = 'UserIdCard/'.$filename;
 		move_uploaded_file($filepath, $destfile);
-
-		
-       
-
-		
+				
 		$select = "SELECT email FROM usertable WHERE email = '$email'";
 		
   		$query = mysqli_query($con, $select);
@@ -45,68 +32,8 @@ if(isset($_POST['submit'])){
 			if ($insertquery) {
 				header("Location: login.php");
 			}
-		}
-
-
-		//$insert = "INSERT Into  usertable ( fname ,  lname,  phone_number ,  email ,  pwd, identity, image  ) VALUES ('$fname', '$lname','$phone_number', '$email', '$pwd', '$identity', '$destfile')";
-		
-
-
-
-		
-
+		}		
 	}
-
 }
-
-
-
-
-/*$lname = $_POST['lname'];
-$phone_number = $_POST['phone_number'];
-$email = $_POST['email'];
-$pwd = $_POST['pwd'];
-$identity = $_POST['identity'];
-$image = $_POST['image'];
-
-$con = mysqli_connect('localhost', 'root', '', 'loginform');
-
-
-
-$filename = $file['name'];
-	$filepath = $file['tmp_name'];
-	$fileerror = $file['error'];
-
-	if($fileerror == 0){
-		
-		$destfile = 'UserIdCard/'.$filename;
-		move_uploaded_file($filepath, $destfile);
-
-	  $select = "SELECT email FROM usertable WHERE email = ? Limit 1";
-
-		$insert = "INSERT Into  usertable ( fname ,  lname,  phone_number ,  email ,  pwd ,  identity ,  image ) VALUES (?,?,?,?,?,?,?)";
-
-		$stmt = $con->prepare($select);
-		$stmt->bind_param("s" ,$email);
-		$stmt->execute();
-		$stmt->bind_result($email);
-		$stmt->store_result(); 
-		$rnum = $stmt->num_rows;
-		if($rnum == 0){
-			$stmt->close();
-
-			$stmt = $con->prepare($insert);
-			$stmt->bind_param("ssissss", $fname, $lname,$phone_number, $email, $pwd, $identity, $destfile);
-			$stmt->execute();
-			header('location: index.html');
-		}else{
-			echo"already registered account";
-		}
-		$stmt->close();
-		$con->close();
-
- }
-}*/
-
 ?>
 
